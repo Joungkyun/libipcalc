a0ffa723c801d3c2326b677d2b209c9577fc5c8a
JoungKyun Kim <joungkyun@gmail.com>
2016-07-09 17:41:47 +0900

    update copyright

--- ipcalc.c
+++ ipcalc.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2011 JoungKyun.Kim <http://oops.org/>
+ *  Copyright (c) 2016 JoungKyun.Kim <http://oops.org>
  *
  *  This file is part of libipcalc.
  *
@@ -15,8 +15,6 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * $Id$
  */
 
 /**
@@ -28,7 +26,7 @@
  * @author	JoungKyun.Kim <http://oops.org>
  * $Date$
  * $Revision$
- * @attention   Copyright (c) 2011 JoungKyun.Kim all rights reserved.
+ * @attention   Copyright (c) 2016 JoungKyun.Kim all rights reserved.
  */
 
 #ifdef HAVE_CONFIG_H
--- utils/util.h
+++ utils/util.h
@@ -1,6 +1,22 @@
 /*
- * $Id$
+ *  Copyright (c) 2016 JoungKyun.Kim <http://oops.org>
+ *
+ *  This file is part of libipcalc.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef UTILAPI_H
 #define UTILAPI_H
 
